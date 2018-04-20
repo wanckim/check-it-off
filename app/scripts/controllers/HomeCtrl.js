456b6eae366baa0bbb35631099bfed1b318804b9
Wan Cheul Kim <wckim88@gmail.com>
2018-04-19 18:01:13 -0700

    Generated tasks are successfully synced with Firebase

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,21 @@
 (function() {
-  function HomeCtrl() {
+  function HomeCtrl($scope, $firebaseArray) {
+    var ref = firebase.database().ref().child("tasks");
 
+    $scope.tasks = $firebaseArray(ref);
+
+    $scope.tasks.$add({
+      text: "Do the laundry"
+    });
+    $scope.tasks.$add({
+      text: "Do the dishes"
+    });
+    $scope.tasks.$add({
+      text: "Do the homework"
+    });
   }
 
   angular
     .module('checkItOff')
-    .controller('HomeCtrl', [HomeCtrl]);
+    .controller('HomeCtrl', ['$scope', '$firebaseArray', HomeCtrl]);
 })();
