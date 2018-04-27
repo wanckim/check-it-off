f32dfea4cb4e650332f2aae649b1a855fc369d8c
Wan Cheul Kim <wckim88@gmail.com>
2018-04-26 22:01:37 -0700

    Tasks that are passed expiration date are hidden from tasks list

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,24 @@
 (function() {
   function HomeCtrl(Task) {
+    // Task.add("testing 1");
+
     /**
     * @desc Grab all task items in Task object firebase array
     * @type {Object}
     */
     this.tasks = Task.all;
+
+    this.isExpired = function(task) {
+      var currentDate = new Date();
+      var expireDate = new Date(task.expireAt);
+      if (currentDate > expireDate) {
+        console.log(task.task + " - expires on " + task.expireAt + " | Expired");
+        return true;
+      } else {
+        console.log(task.task + " - expires on " + task.expireAt + " | On going");
+        return false;
+      };
+    };
   }
 
   angular
--- app/scripts/services/Task.js
+++ app/scripts/services/Task.js
@@ -6,6 +6,39 @@
 
     Task.all = list;
 
+    Task.add = function(task) {
+      var timeArr = getTime();
+      var newTask = {
+        task: task,
+        createdAt: timeArr[0],
+        expireAt: timeArr[1]
+      }
+
+      list.$add(newTask).then(function(ref) {
+        var id = ref.key;
+        list.$indexFor(id);
+      });
+    };
+
+    var getTime = function() {
+      var currentTime = new Date();
+
+      var expiration = new Date();
+      expiration.setDate(expiration.getDate() + 7);
+
+      var createdAt = (currentTime.getMonth() + 1) + "-" +
+                       currentTime.getDate() + "-" +
+                       currentTime.getFullYear();
+
+      var expireAt = (expiration.getMonth() + 1) + "-" +
+                      expiration.getDate() + "-" +
+                      expiration.getFullYear();
+
+      var timeStamp = [createdAt, expireAt];
+
+      return timeStamp;
+    };
+
     return Task;
   }
 
